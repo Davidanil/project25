22b8fe18ae20ebf533a5dd13483010816b543d01
Davidanil <davidanil@hotmail.com>
2017-11-08 13:09:33 +0000

    Search vulns

--- Node.java
+++ Node.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.bcel.internal.generic.PUTSTATIC;
+
 public class Node {
     private List<Node> _children = new ArrayList<Node>();
     private Node _parent = null;
     private String _kind = "root";
     private int _lvl = 0;
     private String _text = "";
+    private String _name = "";
     private boolean _vuln=false;
     	
     //Constructor
@@ -16,6 +19,14 @@ public class Node {
     }
     
     //Get Set
+   
+    public String getName() {
+    	return _name;
+    }
+    
+    public void setName(String name) {
+    	_name = name;
+    }
     
     public boolean getVuln() {
     	return _vuln;
@@ -77,11 +88,23 @@ public class Node {
 
     }
     
+    public String findName(String text) {
+    	String array[] = text.split("\"");
+    	for(int i=0; i<array.length ;i++) {
+    		if(array[i].contains("name")) {
+    			return array[i+2];
+    			}
+    		}
+		return "";
+    }
+    
     public String parentSearch(Node start, String parent, String child) {
     	if (start.getKind().contains(parent))
 			for(Node n : start.getChildren())
-				if(n.getKind().contains(child))
-					return n.getKind() + "\n" + n.getText();
+				if(n.getKind().contains(child)) {
+					n.setName(n.findName(n.getText()));
+					return n.getKind() + "\n" + n.getName();
+					}
     	return "";
     }
     
@@ -105,8 +128,9 @@ public class Node {
     	addVuln(getText().contains("_GET"));
     	if (get_parent()!= null)
     		System.out.println("Parent: " + get_parent().getKind());
-    	System.out.println("Level: " + getLvl() + "\nNode: " + getKind() + getText());
-    	System.out.println(getVuln());
+    	System.out.println("Level: " + getLvl());
+    	System.out.println("Node: " + getKind());
+    	System.out.println("Vuln: " + getVuln());
     	if (getVuln())
     		System.out.println(searchTree(this));
     	System.out.println();
